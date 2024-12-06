44ca43f0e4ba68e7bd674503e371ebd461256859
devdong9897 <dev.dong9897@gmail.com>
2024-12-07 03:31:40 +0900

    feat: 두 수의 차

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 // 두 수의 합
+// function solution(num1, num2) {
+//   return num1 + num2;
+// }
+
+// solution(2, 3);
+// solution(100, 2);
+
+// 두 수의 차
 function solution(num1, num2) {
-  return num1 + num2;
+  return num1 - num2;
 }
 
 solution(2, 3);
